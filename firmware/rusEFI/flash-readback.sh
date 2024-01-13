cee326d7671e60d744baedf8a4dee6644361c1dd
rusefillc <rusefillc@gmail.com>
2024-01-13 16:30:46 -0500

    progress

--- firmware/rusEFI/flash-readback.sh
+++ firmware/rusEFI/flash-readback.sh
@@ -4,7 +4,9 @@ openocd -f interface/jlink.cfg -c 'transport select swd' -f ./at32f4x.cfg \
 	-c "init" \
 	-c "halt" \
 	-c "flash probe 0" \
-	-c "flash read_bank 0 readback-fw.bin" \
+	-c "flash read_bank 0 readback-fw-bank0.bin" \
+	-c "flash probe 1" \
+	-c "flash read_bank 1 readback-fw-bank1.bin" \
 	-c "flash probe 2" \
 	-c "flash read_bank 2 readback-fuses.bin" \
 	-c "reset" \
